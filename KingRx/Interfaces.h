bcb5de5b23dfe490b7193c90d5e4eb879950969c
祐介 富岡 <tomioka2012@gmail.com>
2019-01-03 00:52:31 +0900

    correction Interfaces

--- KingRx/Interfaces.h
+++ KingRx/Interfaces.h
@@ -7,24 +7,26 @@ namespace rx
 	class IDisposable
 	{
 	public:
+		virtual ~IDisposable() {};
 		virtual void Dispose() = 0;
 	};
 
-	template<typename T>
+	template<typename T, typename TException = std::exception>
 	class IObserver
 	{
 	public:
-		virtual ~IObserver() = 0;
+		virtual ~IObserver() {};
 		virtual void OnNext(const T& value) = 0;
-		virtual void OnError(std::unique_ptr<std::exception> error) = 0;
-		virtual void OnComprated() = 0;
+		virtual void OnError(std::unique_ptr<TException> error) = 0;
+		virtual void OnCompleted() = 0;
+		virtual bool operator==(const IObserver<T, TException>& other) = 0;
 	};
 
 	template<typename T>
 	class IObservable
 	{
 	public:
-		virtual ~IObservable() = 0;
-		std::shared_ptr<IDisposable> Subscribe(std::unique_ptr<IObserver> observer) = 0;
+		virtual ~IObservable() {};
+		virtual std::unique_ptr<IDisposable> Subscribe(std::shared_ptr<IObserver<T>> observer) = 0;
 	};
 }
